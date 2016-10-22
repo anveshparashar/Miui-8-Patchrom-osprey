0d09a94025e1898428432fe528a4ba5918eb389b
anveshparashar <anveshparashar@gmail.com>
2016-10-22 20:05:51 +0530

    Add files via upload

---
+++ phoneapps.mk
@@ -0,0 +1,35 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	Bluetooth \
+	BluetoothExt \
+	BluetoothMidiService \
+	CertInstaller \
+	HTMLViewer\
+	KeyChain \
+	LiveWallpapers \
+	PacProcessor \
+	PrintSpooler \
+	LatinIME \
+	Stk \
+	telresources \
+	TimeService \
+        UserDictionaryProvider\
+	WAPPushManager
+
+private-phone-priv-apps := \
+	BackupRestoreConfirmation \
+        CarrierConfig\
+	CellBroadcastReceiver \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	ProxyHandler \
+	qcrilmsgtunnel \
+	SharedStorageBackup \
+	Shell \
+	Telecom \
+	TelephonyProvider \
+	TeleService \
